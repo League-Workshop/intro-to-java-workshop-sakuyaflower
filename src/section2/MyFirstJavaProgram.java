cfe14b74a9e872f998d88081b199aa530f26659c
sakuyaflower <league@WTS10.attlocal.net>
2018-03-17 12:29:54 -0700

    Walking Robot
    I made a robot that can walk in a circle while drawing.

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,82 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
-		
+	Robot Hii=new Robot();
+	Hii.penDown();
+	Hii.setPenColor(100, 3, 120);
+	Hii.setSpeed(10000);
+	Hii.move(-200);
+	Hii.setSpeed(10000);
+
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);	
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);	
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);	
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);	
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);	
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);	
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+	Hii.turn(10);Hii.move(20);
+
 	}
 }
