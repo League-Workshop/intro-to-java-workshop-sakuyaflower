0c30b64bdac0ef0c95e1ec8a10d4d99cb2dca4ab
sakuyaflower <37465491+sakuyaflower@users.noreply.github.com>
2018-03-18 12:50:43 -0700

    What is this?
    We made a robot that can walk in circles. Also made a game and made the computer talk back to you.

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,22 +1,27 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		      int score=0;   
 		// 2.  Ask the user a question 
-		
+		      String answer=JOptionPane.showInputDialog("Who are you?");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		    		  if (answer.equals(42)) {
+				    	  score++;}
 		// 4.  if the user's answer is correct
-		
+		    		  JOptionPane.showInputDialog(null,score);
 		// -- add one to their score 
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
 		
+		    if (answer.equals("Whats my name?")); {
+			score++;}
 		// 6.  After all the questions have been asked, print the user's score 
-		
+		JOptionPane.showInputDialog(null,score);
 	}
 }
