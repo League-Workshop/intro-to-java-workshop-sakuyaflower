47b7f77dc2da42d5025121438952731396eea813
sakuyaflower <league@WTS10.attlocal.net>
2018-03-24 12:21:58 -0700

    Games
    We made a dragon fight that took forever. But it was really fun. Also
    we made a scary maze that we will continue tomorrow.

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -1,13 +1,30 @@
 package section4;
 
 import java.util.Random;
-
+import javax.swing.JOptionPane; 
 public class AwesomeOrNot {
+public static void main(String[] args){
+	int num=new Random().nextInt(4);
+JOptionPane.showMessageDialog(null,num);
+
+String a = JOptionPane.showInputDialog(null, "Enter something that is awsome");
+if(num==0) {	
+	JOptionPane.showMessageDialog(null,a+ "is awesome");
+}
+else if(num==1) {
+	JOptionPane.showMessageDialog(null,a+"is ok");
+}
+else if(num==2) {
+	JOptionPane.showMessageDialog(null,a+"is boring");
+}
+else if(num==3) {
+	JOptionPane.showMessageDialog(null,a+"is weird");
+}
 
 	// 1. Make a main method that includes all the steps below…
-
+   
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+				
 	// 3. Print out this variable
 
 	// 4. Get the user to enter something that they think is awesome
@@ -29,5 +46,5 @@ public class AwesomeOrNot {
 	// -- write your own answer
 
 }
-
+}
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -9,7 +9,7 @@ public class QuizGame {
 		// 1.  Create a variable to hold the user's score 
 		      int score=0;   
 		// 2.  Ask the user a question 
-		      String answer=JOptionPane.showInputDialog("Who are you?");
+		      String answer=JOptionPane.showInputDialog("How old are you?");
 		// 3.  Use an if statement to check if their answer is correct
 		    		  if (answer.equals(42)) {
 				    	  score++;}
@@ -19,7 +19,8 @@ public class QuizGame {
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
 		
-		    if (answer.equals("Whats my name?")); {
+		    		  answer=JOptionPane.showInputDialog("Whats my name?)");
+		    		  if (answer.equals("Bob")); {
 			score++;}
 		// 6.  After all the questions have been asked, print the user's score 
 		JOptionPane.showInputDialog(null,score);
